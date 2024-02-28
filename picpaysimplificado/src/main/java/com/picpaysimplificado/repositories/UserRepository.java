de295a816497e1c017101aa85cbc9de8b29d75e0
Jonathas <jtsbarrosferreira@gmail.com>
2024-02-27 23:21:38 -0300

    Change to Interface

--- picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository implements JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository <User, Long> {
+
     Optional<User> findUserByDocument(String document);
 
-    Optional<User> findUserByDocument(Long id);
+    Optional<User> findUserById(Long id);
 }
