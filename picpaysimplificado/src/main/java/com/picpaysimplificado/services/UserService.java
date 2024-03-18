293c377021e012eaf6c0d92894c2b3c9a038d14d
Jonathas <jtsbarrosferreira@gmail.com>
2024-03-18 12:16:01 -0300

    create UserService

--- picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -15,7 +17,7 @@ public class UserService {
     private UserRepository repository;
 
     public void validateTransaction(User sender, BigDecimal amount) throws Exception {
-        if(sender.getUserType() == UserType.COMMON){
+        if(sender.getUserType() == UserType.MERCHANT){
             throw new Exception("Usuário do tipo Lojista não está autorizado a realizar transação");
         }
 
@@ -28,6 +30,15 @@ public class UserService {
 
     }
 
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+       return this.repository.findAll();
+    }
     public void saveUser(User user){
         this.repository.save(user);
     }
